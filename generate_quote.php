20e3dcddfab2abd098da48cdddae54375d3294d4
Jason Douglas <jdouglas71@gmail.com>
2020-05-28 09:43:38 -0700

    Updated tests with more params

--- generate_quote.php
+++ generate_quote.php
@@ -3,32 +3,34 @@
 require_once( "call_api.php" );
 
 $base_url = "http://api-staging.ripcordrescuetravelinsurance.com";
-$endpoint = "/api/quote/generate_quote";
+$endpoint = "/api/quote/generate_quote.php";
 
 $data = array( "resident_country" => "US",
-			   "resident_state" => "FL",
+			   "resident_state" => "MO",
 			   "destination_country" => "BN",
 			   "travel_insurance_type" => "2",
 			   "evac_type" => "2",
+			   "cancel_policy" => "false",
 			   "traveler_first_name" => array("Jason","Billy"),
 			   "traveler_last_name" => array("Douglas","Boggs"),
 			   "traveler_dob" => array("12/12/1987","2/2/1946"),
-			   "traveler_trip_cost" => array("3456","33423"),
+			   "traveler_trip_cost" => array("3456","3423"),
 			   "trip_start_date" => "9/29/2020",
 			   "trip_end_date" => "10/9/2020",
 			   "initial_deposit_date" => date("m/d/Y"),
-			   "street_address" => "",
-			   "city" => "",
-			   "state" => "",
-			   "zip" => "",
+			   "program" => "cavalry",
+			   "api_token" => "thisisatoken",
+			   "quoted_price" => "",
+			   "street_address" => "123 C Street",
+			   "city" => "Santa Cruz",
+			   "state" => "CA",
+			   "zip" => "95060",
 			   "cell_phone" => "",
 			   "home_phone" => "",
-			   "email" => "",  
-			   "program" => "cavalry",
-			   "agent_id" => "test_account",
-			   "api_token" => "thisisatoken",
+			   "email" => "test@test.com",
 			 );
 
+echo json_encode($data).PHP_EOL;
 echo call_api( $base_url.$endpoint, json_encode($data) ).PHP_EOL;
 
 ?>
--- login.php
+++ login.php
@@ -3,12 +3,14 @@
 /** Test for the agent_login endpoint */
 require_once( "call_api.php" );
 
-$base_url = "http://api-staging.ripcordrescuetravelinsurance.com";
-$endpoint = "/api/agent/agent_login";
+$base_url = "https://api-staging.ripcordrescuetravelinsurance.com";
+$endpoint = "/api/agent/agent_login.php";
 
 $data = array( "agent_filter" => "test_account",
 			   "agent_username" => "test@account.com",
 			   "agent_password" => "password" );
+			   
+echo json_encode($data).PHP_EOL;
 
 echo call_api( $base_url.$endpoint, json_encode($data) );
 
