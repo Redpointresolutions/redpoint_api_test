ad2e5e4280685d9350dd7d80392a646b92e7b5e1
Jason Douglas <jdouglas71@gmail.com>
2021-06-15 06:54:55 -0700

    updates

--- update_policy.php
+++ update_policy.php
@@ -3,6 +3,7 @@
 require_once( "call_api.php" );
 
 $base_url = "http://ripcord-api.test";
+$base_url = "https://api-staging.redpointtravelprotection.com";
 $endpoint = "/api/agent/agent_login.php";
 
 $data = array( "agent_id" => "test_account",
@@ -14,8 +15,9 @@ $result = json_decode($result,true);
 
 $endpoint = "/api/policy/update_policy.php";
 
-$data = array( "redpoint_policy_number" => "UOI202150509",
-			   "trip_cost_updates" => array("500"),
+$data = array( "redpoint_policy_number" => "UOI202150585",
+			   //"trip_cost_updates" => array("500"),
+			   "traveler_trip_cost" => array("6000","6000"),
 			   "send_email" => "true",
 			   "api_token" => $result['api_token'],
 			 );
